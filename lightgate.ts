c3d08c06061a9e3beb71e19f25089598dec59ac5
Tomáš Kazda <36702826+TomasKazda@users.noreply.github.com>
2022-02-26 11:00:20 +0100

    ultrasionic ping

--- lightgate.ts
+++ lightgate.ts
@@ -58,4 +58,27 @@ namespace RunComp {
             }
         })
     }
+
+    let sosCoef = 1 / ((331 + 0.607 * (input.temperature() - 4)) / 10000 / 2)
+    /**
+     * Send a ping and get the echo time (in microseconds) as a result
+     * @param trig tigger pin
+     * @param echo echo pin
+     * @param unit desired conversion unit
+     * @param maxCmDistance maximum distance in centimeters (default is 500)
+     */
+    //% blockId="sensor_ping" block="ping trig %trig|echo %echo|unit %unit"
+    function ping(trig: DigitalPin, echo: DigitalPin, maxCmDistance = 500): number {
+        // send pulse
+        pins.setPull(trig, PinPullMode.PullNone);
+        pins.digitalWritePin(trig, 0);
+        control.waitMicros(2);
+        pins.digitalWritePin(trig, 1);
+        control.waitMicros(10);
+        pins.digitalWritePin(trig, 0);
+
+        // read pulse in cm
+        const d = pins.pulseIn(echo, PulseValue.High, maxCmDistance * 58);
+        return Math.round(d / sosCoef)
+    }
 }
